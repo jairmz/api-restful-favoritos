5182c081fd0b0790305f19c6246df47e2090ed58
Jairmz <jairmz@hotmail.com>
2018-03-09 00:30:49 +0100

    mongo atlas

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ var port = process.env.PORT || 3678;
 
 
 /*mongoose.connect('mongodb://localhost:27017/cursofavoritos',(err,res) => {*/
-mongoose.connect('mongodb://root:testmongo@ds131510.mlab.com:31510/apifavoritos',(err,res) => {
+/*mongoose.connect('mongodb://root:testmongo@ds131510.mlab.com:31510/apifavoritos',(err,res)*/
+mongoose.connect('mongodb://root:testmongo@jair-test-shard-00-00-u9jao.mongodb.net:27017,jair-test-shard-00-01-u9jao.mongodb.net:27017,jair-test-shard-00-02-u9jao.mongodb.net:27017/test?ssl=true&replicaSet=jair-test-shard-0&authSource=admin',(err,res) => {
 	if(err) {
 		throw err;
 	} else {
